a09ab2bb063cc543a90ae27a7c22fa2e9dc8cd56
dock1989 <durgesh13june1989@gmail.com>
2024-11-15 21:03:36 +0530

    Adding Looping class

--- src/main/java/inputloopingcondition/Looping.java
+++ src/main/java/inputloopingcondition/Looping.java
@@ -1,2 +1,44 @@
-package inputloopingcondition;public class Looping {
+package inputloopingcondition;
+
+import util.ScannerUtil;
+
+public class Looping {
+    public static void main(String[] args) {
+        Looping looping = new Looping();
+        System.out.println("Enter a number");
+        int a = ScannerUtil.takeInput().nextInt();
+        looping.forLoopExm(a);
+        looping.whileLoopExm(a);
+        looping.dowhileLoopExm(a);
+    }
+
+    public void forLoopExm(int a){
+        System.out.println("From for loop");
+        for (int i=0;i<=10;i++){
+            System.out.println(i*a);
+        }
+
+    }
+
+    public void whileLoopExm(int a){
+        System.out.println("From while loop");
+        int i=0;
+        while (i<=10){
+            System.out.println(i*a);
+            i++;
+        }
+
+    }
+
+    public void dowhileLoopExm(int a){
+        System.out.println("From do-while loop");
+        int i=0;
+        do{
+            System.out.println(i*a);
+            i++;
+        }
+        while (i<=10);
+
+    }
+
 }
