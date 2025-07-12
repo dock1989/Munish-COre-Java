045fdbc009188d73edaedeae5a68447b3bfd0b86
dock1989 <durgesh13june1989@gmail.com>
2025-07-12 11:33:23 +0530

    commit on 12-07

---
+++ src/main/java/array/ArrayDeclration.java
@@ -0,0 +1,37 @@
+package array;
+
+import java.util.Scanner;
+
+/**
+ * <b>Addition of 10 numbers passed in an Array</b>
+ */
+
+public class ArrayDeclration {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a[] = new int[10];
+        int sum = 0;
+        System.out.println("a length " + a.length);
+
+        int b[] = new int[]{1,2,3,4,5,6,7,8};
+        System.out.println(b.length);
+
+//        for(int i=0;i<10;i++){
+//            System.out.println("Enter a number");
+//            a[i] = scanner.nextInt();
+//            sum += a[i];
+//        }
+
+//        System.out.println(sum);
+
+        for(int i=0;i<b.length;i++){
+            if(b[i] % 2 == 0)
+                System.out.println("The even number is " + b[i]);
+            else
+                System.out.println("The odd number is " + b[i]);
+        }
+
+
+
+    }
+}
--- src/main/java/array/EvenOutOfTenNumber.java
+++ src/main/java/array/EvenOutOfTenNumber.java
@@ -7,17 +7,23 @@ public class EvenOutOfTenNumber {
         int a [] = new int[10];
         int count =0;
         Scanner scanner = new Scanner(System.in);
-        for(int i=0;i<10;i++){
-            System.out.println("Enter any number");
-            a[i] = scanner.nextInt();
-        }
-        for(int i=0;i<10;i++){
-            if(a[i] % 2 ==0) {
-                System.out.println(a[i]);
-                count++;
+        char opt;
+        do {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Enter any number");
+                a[i] = scanner.nextInt();
             }
+            for (int i = 0; i < 10; i++) {
+                if (a[i] % 2 == 0) {
+                    System.out.println(a[i]);
+                    count++;
+                }
+            }
+            System.out.println("Total even numbers are " + count);
+            System.out.println("Do you want to process more say Y/N");
+            opt = scanner.next().charAt(0);
         }
-        System.out.println("Total even numbers are " + count);
+        while (opt=='Y' || opt == 'y');
 
     }
 }
